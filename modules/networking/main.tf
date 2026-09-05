c4fb58f16951acdfa4e4f70c9fc7b45a6c33e3f9
MantasKr1 <mantaskryzevicius1@gmail.com>
2026-09-05 16:08:28 +0300

    Refactor formatting in networking module for improved readability of public subnet resource definitions.

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -19,9 +19,9 @@ resource "aws_internet_gateway" "this" {
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
 
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zones[count.index]
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
   # ALB/NAT use their own addresses; do not auto-assign public IPs to random instances.
   map_public_ip_on_launch = false
 
